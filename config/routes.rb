9deefab47b5ef3efa4c870e75e75c67ddd5abe23
ralles <ralles.liu@gmail.com>
2021-01-19 15:45:22 -0500

    finish initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # not too sure how this works, but it seems like 'root' is in to mean '/'
+  # and it tells to go to the controller at static_pages
+  # then the controller default renders the root.html.erb view  
+  root to: 'static_pages#root'
 end
+  
