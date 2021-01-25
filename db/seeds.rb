7bbcf4f81b793b5ddf3b30db2f08bd70540b9f1d
ralles <ralles.liu@gmail.com>
2021-01-24 23:22:59 -0500

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.delete_all
+Channel.delete_all
+ChannelMembership.delete_all
+
+
+demo = User.create!(
+    display_name: "demo",
+    username: "demo@gmail.com",
+    password: "demopassword"
+)
